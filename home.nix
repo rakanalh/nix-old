596dd015c1fb59dfb61707e12c2415341297871b
rakanalh <rakan.alhneiti@gmail.com>
2022-11-18 00:59:20 +0300

    Example user service

--- home.nix
+++ home.nix
@@ -554,4 +554,24 @@ in {
       notify = true;
     };
   };
+
+  # systemd.user.services.dropbox = {
+  #   Unit = {
+  #     Description = "Dropbox";
+  #     After = [ "graphical-session-pre.target" ];
+  #     PartOf = [ "graphical-session.target" ];
+  #   };
+
+  #   Service = {
+  #     Restart = "on-failure";
+  #     RestartSec = 1;
+  #     ExecStart = "${pkgs.dropbox}/bin/dropbox";
+  #     Environment = "QT_PLUGIN_PATH=/run/current-system/sw/${pkgs.qt5.qtbase.qtPluginPrefix}";
+  #    };
+
+  #   Install = {
+  #       WantedBy = [ "graphical-session.target" ];
+  #   };
+
+  # };
 }
